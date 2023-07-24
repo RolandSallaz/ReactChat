e2b0660744aa90dcc79c3e120b7a52dc6a52e44c
Roland <machodin@yandex.ru>
2023-07-24 17:47:42 +0300

    fix api url

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
 import './index.css'
-import {BrowserRouter} from "react-router-dom";
-
-const isDev = import.meta.env.DEV
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <BrowserRouter basename={isDev ? '' : 'ReactChat'}>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
 )
